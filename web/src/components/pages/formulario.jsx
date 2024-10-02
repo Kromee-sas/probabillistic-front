d17ed9aa389628e23d2a37f4699c94804e0d80ad
AgustinReal <agustinreal26@gmail.com>
2024-10-02 19:10:17 -0300

    Se agrego dependencias y se creo el componente formulario.

---
+++ web/src/components/pages/formulario.jsx
@@ -0,0 +1,133 @@
+export function Formulario() {
+
+    return (
+        <footer>
+            <h2>Orden de producción</h2>
+            <form action="">
+                <section>
+                    <legend>Información del Cliente</legend>
+                    <div>
+                        <input type="text" />
+                        <label htmlFor="">Cliente</label>
+                    </div>
+                    <div>
+                        <input type="text" />
+                        <label htmlFor="">Vehiculo</label>
+                    </div>
+                    <div>
+                        <input type="text" />
+                        <label htmlFor="">Placa</label>
+                    </div>
+                    <div>
+                        <input type="text" />
+                        <label htmlFor="">Chasis</label>
+                    </div>
+                    <div>
+                        <input type="text" />
+                        <label htmlFor="">Color</label>
+                    </div>
+                    <div>
+                        <input type="text" />
+                        <label htmlFor="">Nivel de blindaje</label>
+                    </div>
+                </section>
+                <h2>Expecificaciones interveción de seguridad</h2>
+                <section>
+                    <legend>Cristales</legend>
+                    <div>
+                        <label htmlFor="">Proveedor</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Color cristales</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Espesor</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Apertura de vidrios</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Marco en acero petañas</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+
+                    </div>
+                    <div>
+                        <label htmlFor="">Sun Rof con cristal blindado</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                </section>
+                <section>
+                    <legend>Cristales</legend>
+                    <div>
+                        <label htmlFor="">Proveedor</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Color cristales</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Espesor</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Apertura de vidrios</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Marco en acero petañas</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Sun Rof con cristal blindado</label>
+                        <select name="" id="">
+                            <option value="">Si</option>
+                            <option value="">No</option>
+                        </select>
+                    </div>
+                </section>
+                <div>
+                    <button>Enviar</button>
+                    <button>Limpiar campos</button>
+                </div>
+            </form>
+        </footer>
+    )
+}
